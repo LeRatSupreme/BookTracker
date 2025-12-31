885309d64372538acb4f1413924a1f60c800ef94
LeRatSupreme <rnr6.elf@gmail.com>
2025-12-31 10:15:55 +0100

    BUG : Page principal non fonctionnel

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -35,17 +35,7 @@ const Layout = () => {
 
             {/* Main Content Area */}
             <main className="flex-1 overflow-y-auto px-4 sm:px-6 relative pb-28">
-                <AnimatePresence mode="wait">
-                    <motion.div
-                        key={location.pathname}
-                        initial={{ opacity: 0, y: 10 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        exit={{ opacity: 0, y: -10 }}
-                        transition={{ duration: 0.2 }}
-                    >
-                        <Outlet />
-                    </motion.div>
-                </AnimatePresence>
+                <Outlet />
             </main>
 
             {/* Bottom Navigation (Floating Island) */}
